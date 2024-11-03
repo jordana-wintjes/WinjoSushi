afc3e04b2be40386f13fa7e62d12a8c969bd3313
jordana-wintjes <67658555+jordana-wintjes@users.noreply.github.com>
2024-11-03 15:10:58 -0700

    Add JSON and modal creation

--- scripts.js
+++ scripts.js
@@ -23,5 +23,55 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll('.tab-pane').forEach(function (pane) {
             observer.observe(pane, { attributes: true });
         });
+
+        // Load the JSON data
+        fetch('menuItems.json')
+            .then(response => response.json())
+            .then(data => {
+                // Add click event listeners to the cards
+                document.querySelectorAll('.card').forEach(function (card, index) {
+                    card.addEventListener('click', function () {
+                        var item = data[index];
+                        showModal(item);
+                    });
+                });
+            });
+
+        // Function to show the modal with item details
+        function showModal(item) {
+            var modalContent = `
+                <div class="modal fade" id="itemModal" tabindex="-1" role="dialog" aria-labelledby="itemModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="itemModalLabel">${item.name}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p><strong>Description:</strong> ${item.description}</p>
+                                <p><strong>Ingredients:</strong> ${item.ingredients.join(', ')}</p>
+                                <p><strong>Price:</strong> ${item.price}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            document.body.insertAdjacentHTML('beforeend', modalContent);
+            $('#itemModal').modal('show');
+
+            // Adjust padding-right when modal is shown
+            document.body.style.paddingRight = '0px';
+
+            $('#itemModal').on('hidden.bs.modal', function () {
+                document.getElementById('itemModal').remove();
+                // Reset padding-right when modal is hidden
+                document.body.style.paddingRight = '';
+            });
+        }
     }
 });
